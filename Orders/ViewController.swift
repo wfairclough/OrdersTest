b8f876ced27d726891e71a0aaa2d61716aeddb2a
Will Fairclough <will.fairclough@suitespottechnology.com>
2018-05-06 16:26:52 -0400

    Initial Commit

---
+++ Orders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Orders
+//
+//  Created by Will Fairclough on 2018-05-06.
+//  Copyright © 2018 Will Fairclough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
